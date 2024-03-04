a8b4fb2ff0a40260559185ccdd4627f28caf9bc5
XackiGiFF <m.girsanoff@gmail.com>
2024-03-04 14:36:37 +0300

    First

---
+++ src/MPECoders/PluginTemplate/PluginName.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ *       ███╗   ███╗██████╗ ███████╗     ██████╗ ██████╗ ██████╗ ███████╗██████╗ ███████╗
+ *       ████╗ ████║██╔══██╗██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗██╔════╝
+ *       ██╔████╔██║██████╔╝█████╗      ██║     ██║   ██║██║  ██║█████╗  ██████╔╝███████╗
+ *       ██║╚██╔╝██║██╔═══╝ ██╔══╝      ██║     ██║   ██║██║  ██║██╔══╝  ██╔══██╗╚════██║
+ *       ██║ ╚═╝ ██║██║     ███████╗    ╚██████╗╚██████╔╝██████╔╝███████╗██║  ██║███████║
+ *       ╚═╝     ╚═╝╚═╝     ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * В случае неисправности плагина, просим обратиться к нам в личные сообщения VK.
+ * Мы VK: vk.com/mpe_coders
+ * Желаем приятного использования плагина!
+*/
+
+declare(strict_types=1);
+
+namespace MPECoders\PluginTemplate;
+
+use pocketmine\plugin\PluginBase;
+
+class PluginName extends PluginBase{
+
+    public function onLoad() : void{
+        parent::onLoad();
+    }
+
+    public function onEnable() : void{
+        parent::onEnable();
+    }
+
+    public function onDisable() : void{
+        parent::onDisable();
+    }
+
+}
